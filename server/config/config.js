7357ab10a2c9c30fe8accbd2456691651ed1a8b6
sanjeevsky <knit16643@gmail.com>
2019-02-13 13:17:00 +0530

    Update config to use config.json

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,10 @@
-var env=process.env.NODE_ENV || 'development';
-if(env==='development')
-{
-  process.env.port=3000;
-  process.env.MONGODB_URI='mongodb://localhost:27017/TodoApp';
-}
-else if(env==='test')
-{
-process.env.port=3000;
-process.env.MONGODB_URI='mongodb://localhost:27017/TodoAppTest';
+var env = process.env.NODE_ENV || 'development';
+
+if (env === 'development' || env === 'test') {
+  var config = require('./config.json');
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
 }
